66a057b123ce163070db10c8c9612a31c7a00905
babyo77 <onlybabyyo7@gmail.com>
2024-06-07 01:14:20 +0530

    update

--- src/components/ui/download.tsx
+++ src/components/ui/download.tsx
@@ -47,7 +47,7 @@ function Download() {
             height={500}
             width={500}
             priority
-            blurDataURL="/warn.webp"
+            placeholder="blur"
             alt="code-signed-warning"
           />
         </div>
