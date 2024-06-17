bb316c8318d5958b60c9d38ee46af325247e4129
babyo77 <onlybabyyo7@gmail.com>
2024-06-17 14:00:37 +0530

    v1.0.11

--- src/app/support/page.tsx
+++ src/app/support/page.tsx
@@ -1,13 +1,31 @@
-export default function page() {
+"use client";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
+export default function Page() {
+  const router = useRouter();
+  const [sec, setSec] = useState(4);
+  useEffect(() => {
+    const t = setTimeout(() => {
+      router.push("https://tanmays-organization-2.gitbook.io/ship/");
+    }, 4000);
+    const i = setInterval(() => {
+      setSec((prev) => (prev - 1 + 4) % 4);
+    }, 1000);
+    return () => {
+      clearTimeout(t);
+      clearInterval(i);
+    };
+  }, [router]);
   return (
     <div className="p-4">
-      Coming soon{" "}
       <a
         href="https://github.com/babyo777/ship"
         className="text-blue-500  underline underline-offset-2"
       >
         contribute
       </a>
+      <p>Redirecting... in {sec}</p>
     </div>
   );
 }
