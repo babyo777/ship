0da18c58ae53d26b6389569af1f9edeaf433fd43
babyo77 <onlybabyyo7@gmail.com>
2024-06-18 20:56:55 +0530

    v.1.0.11

--- src/app/support/page.tsx
+++ src/app/support/page.tsx
@@ -10,17 +10,16 @@ export default function Page() {
       router.push("https://tanmays-organization-2.gitbook.io/ship/");
     }, 4000);
     const i = setInterval(() => {
-      if (sec <= 0) return;
       setSec((prev) => (prev - 1 + 4) % 4);
     }, 1000);
     return () => {
       clearTimeout(t);
       clearInterval(i);
     };
-  }, [router, sec]);
+  }, [router]);
   return (
     <div className="p-4">
-      <p>Redirecting... in {sec}</p>
+      <p>Redirecting... in {sec <= 0 ? 0 : sec}</p>
     </div>
   );
 }
